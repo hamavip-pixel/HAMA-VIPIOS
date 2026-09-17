f8c90f9b9a82c8e1e060b0263ae6d9477514b548
hamavip-pixel <gorahawrami2@gmail.com>
2026-09-17 19:32:52 +0300

    Update ⁠main.mm

--- ⁠main.mm
+++ ⁠main.mm
@@ -1,26 +1,41 @@
 #import <UIKit/UIKit.h>
 
-__attribute__((constructor)) static void HamaVipWelcome() {
+__attribute__((constructor)) static void HamaVipWelcomeWithLogo() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_main_queue(), ^{
         UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
         if (!keyWindow) {
             keyWindow = [[UIApplication sharedApplication].windows firstObject];
         }
         
-        // دروستکردنی سەرەکی پەنجەرەکە (Alert)
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"HAMA ⚡️ VIP" 
-            message:@"ئەم هاکە دروست کراوی چەناڵی HAMA⚡️VIP یە وەک هاوکاریەک ئەگەر جۆینی چەناڵیت نەکردوە.. بیکە بۆ ئەوەی بێبەش نەبی لەهاکی تر😍❤️" 
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"\n\n\n\nHAMA ⚡️ VIP" 
+            message:@"ئەم هاکە دروست کراوی چەناڵی HAMA⚡️VIP یە وەک هاوکاریەک ئەگەر جۆینی چەناڵیت نەکردوە.. بیکە بۆ ئەوەی بێبەش نەبی لەهاکی تر😍❤️\n\nجۆین کردنی چەناڵ..." 
             preferredStyle:UIAlertControllerStyleAlert];
             
-        // دوگمەی جۆین کردنی چەناڵ
-        UIAlertAction *joinAction = [UIAlertAction actionWithTitle:@"جۆین کردنی چەناڵ 🔗" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        // دابینکردنی لۆگۆیەکی بازنەیی لە سەرەوەی پەنجەرەکە
+        UIView *customView = alert.view;
+        UIImageView *logoView = [[UIImageView alloc] initWithFrame:CGRectMake(customView.bounds.size.width / 2 - 40, 15, 80, 80)];
+        logoView.layer.cornerRadius = 40;
+        logoView.layer.masksToBounds = YES;
+        logoView.layer.borderWidth = 2.0;
+        logoView.layer.borderColor = [UIColor redColor].CGColor;   // چوارچێوەیەکی سور بۆ لۆگۆکە
+        
+        // خوێندنەوەی وێنەکەی لۆگۆ لە بەندرڵی توەکەکە یان دایلیبەکە
+        NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+        NSString *imagePath = [bundle pathForResource:@"logo" ofType:@"png"]; // ناوی وێنەکەی لۆگۆکەت
+        if (imagePath) {
+            logoView.image = [UIImage imageWithContentsOfFile:imagePath];
+        }
+        
+        // ئەگەر وێنەکە لە نەتبووک یان دەشتبەجێ لە ڕێگەیەکی ترە دەتوانە لێرە ببەسترێت
+        [customView addSubview:logoView];
+
+        // دوگمەی جۆین کردنی تێلگرام
+        UIAlertAction *joinAction = [UIAlertAction actionWithTitle:@"🔗 جۆین کردنی چەناڵ" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
             [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://t.me/hama_vip1"] options:@{} completionHandler:nil];
-            // دووبارە نیشاندانەوەی پەنجەرەکە تاوەکو بتنۆرێت دوگمەی بەردەوامبوون بگرێت
-            [keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
         }];
         
-        // دوگمەی بەردەوام بوون (بۆ کردنەوەی هاکەکە)
-        UIAlertAction *continueAction = [UIAlertAction actionWithTitle:@"بەردەوام بوون 🚀" style:UIAlertActionStyleCancel handler:^(BOOL action) {
+        // دوگمەی بەردەوامبوون بۆ چوونە ناو هاکەکە
+        UIAlertAction *continueAction = [UIAlertAction actionWithTitle:@"🚀 بەردەوام بوون" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
             // لێرەدا هاکەکە دەست بە کار دەکات
         }];
         
